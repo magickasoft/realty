4293f20ec8c55f9c645c917997282fcb0070404a
Evgeny Shmakov <sjs-master@yandex.ru>
2022-04-13 04:28:03 +0700

    add configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true,
+    minimumCacheTTL: 60,
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    domains: ['cdn.pixabay.com', 'media.istockphoto.com'],
+  },
 }
 
 module.exports = nextConfig

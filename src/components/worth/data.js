d8843aca621304fb4b14a5834e9ce2f3f10aa8ca
Evgeny Shmakov <sjs-master@yandex.ru>
2022-05-17 00:41:22 +0700

    fix: worth

--- src/components/worth/data.js
+++ src/components/worth/data.js
@@ -1,38 +1,56 @@
 export const list = [
   {
     label: 'Покупка квартиры в Новостройке бесплатно',
-    name: 'call',
-  },
-  {
-    label: 'Получение ипотеки со ставкой от 2%',
-    name: 'call',
-  },
-  {
-    label: 'Дистанционное оформление сделки',
-    name: 'call',
-  },
-  {
-    label: 'Возможность использования материнского капитала',
-    name: 'call',
-  },
-  {
-    label: 'Продажа имеющегося жилья на выгодных условиях',
-    name: 'call',
-  },
-  {
-    label: 'Оформление военной ипотеки',
-    name: 'call',
+    src: '/static/images/worth/1.png',
+    width: 204,
+    height: 169,
   },
   {
     label: 'Полное сопровождение на всех этапах сделки',
-    name: 'call',
+    src: '/static/images/worth/2.png',
+    width: 169,
+    height: 169,
+  },
+  {
+    label: 'Получение ипотеки со ставкой от 2%',
+    src: '/static/images/worth/3.png',
+    width: 169,
+    height: 169,
+  },
+  {
+    label: 'Оформление военной ипотеки',
+    src: '/static/images/worth/4.png',
+    width: 169,
+    height: 169,
+  },
+  {
+    label: 'Возможность использования материнского капитала',
+    src: '/static/images/worth/5.png',
+    width: 169,
+    height: 169,
+  },
+  {
+    label: 'Продажа имеющегося жилья на выгодных условиях',
+    src: '/static/images/worth/6.png',
+    width: 169,
+    height: 169,
   },
   {
     label: 'Семейная Ипотека 4,5%',
-    name: 'call',
+    src: '/static/images/worth/7.png',
+    width: 169,
+    height: 169,
+  },
+  {
+    label: 'Дистанционное оформление сделки',
+    src: '/static/images/worth/8.png',
+    width: 169,
+    height: 169,
   },
   {
     label: 'Трейд Ин (Trade in)',
-    name: 'call',
+    src: '/static/images/worth/9.png',
+    width: 169,
+    height: 169,
   },
 ];
--- src/components/worth/item.js
+++ src/components/worth/item.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import SC from 'styled-components';
+import Image from 'next/image';
 import {Icon} from '../icon';
 import {maxDevice, theme} from '../../theme';
-
-const Container = SC.div`
-  margin: 14px 0;
-  padding: 4px 14px;
-  text-align: center;
-  @media ${maxDevice.mobileL} {
-    margin: 12px 0;
-    padding: 4px 12px;
-  }
-`;
 
 const Label = SC.div`
   font-style: normal;
@@ -21,11 +12,38 @@ const Label = SC.div`
   color: ${theme.colors.Black};
   text-align: center;
   margin-top: 22px;
+  width: 95%;
 `;
 
-export const Item = ({label, name}) => (
+const Img = SC(Image)`
+  object-fit: cover;
+  border: 0;
+`;
+
+const Container = SC.div`
+  transition: all 0.2s ease-in-out;
+  padding: 14px 14px;
+  text-align: center;
+  overflow: hidden;
+  border-radius: 12px;
+  @media ${maxDevice.mobileL} {
+    margin: 12px 0;
+    padding: 4px 12px;
+  }
+  &:hover {
+    transform: translateY(-6px);
+    box-shadow: 0px 3px 15px rgb(0 0 0 / 50%);
+    background: ${theme.colors.Black};
+ 
+  }
+  &:hover ${Label} {
+    color: ${theme.colors.White};
+  }
+`;
+export const Item = ({label, src, name, width, height}) => (
   <Container>
     {name && <Icon name={name} size={94} />}
+    {src && <Img src={src} width={width} height={height} alt={label} />}
     {label && <Label>{label}</Label>}
   </Container>
 );
